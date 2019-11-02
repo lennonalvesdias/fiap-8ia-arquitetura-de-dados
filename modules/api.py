30136dd951c75145c4e9fd176a2b528b9c012483
Lennon Alves Dias <lennonalvesdias@gmail.com>
2019-11-02 01:18:56 -0300

    companies in api

--- modules/api.py
+++ modules/api.py
@@ -1,7 +1,8 @@
-#!/usr/bin/env python
+#!C:\\Users\\lenno\\Anaconda3\\python.exe
 # -*- coding: utf-8 -*-
 
 import requests
+from bs4 import BeautifulSoup
 
 BASE_URL = 'https://www.4devs.com.br/ferramentas_online.php'
 
@@ -9,6 +10,20 @@ def get_peoples(gender, quantity):
     params = {'acao': 'gerar_pessoa', 'sexo': gender, 'txt_qtde': quantity}
     return requests.post(BASE_URL, data=params).json()
 
-def get_companies(state, age):
+def get_company(state, age):
     params = {'acao': 'gerar_empresa', 'pontuacao': 'N', 'estado': state, 'idade': age}
-    return requests.post(BASE_URL, data=params).text
+    companie = requests.post(BASE_URL, data=params).text
+    soup = BeautifulSoup(companie)
+    return {
+        'nome': soup.find(id='nome').get('value'),
+        'cidade': soup.find(id='cidade').get('value'),
+        'estado': soup.find(id='estado').get('value')
+    }
+
+def get_companies(state, age, quantity):
+    i = 0
+    companies = []
+    while i <= quantity:
+        companies += [get_company(state, age)]
+        i += 1
+    return companies
