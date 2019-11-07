b36067340b5a210d9d053ac3a6e055a904adb7f1
Lennon Alves Dias <lennonalvesdias@gmail.com>
2019-11-07 00:19:29 -0300

    fix parameters

--- app.py
+++ app.py
@@ -10,14 +10,15 @@ def main():
     try:
         print('running application')
         print('generate peoples')
-        peoples = people.generate_peoples(30, 20)
+        peoples = people.generate_peoples(5, 5)
         print('saving peoples')
         helper.save_txt('data/peoples.txt', peoples)
         print('generate companies')
-        companies = company.generate_companies(30)
+        companies = company.generate_companies(6)
         print('saving companies')
+        helper.save_txt('data/companies.txt', companies)
         print('generate universities')
-        universities = university.generate_universities(20)
+        universities = university.generate_universities(4)
         print('saving universities')
         helper.save_txt('data/universities.txt', universities)
     except Exception as ex:
